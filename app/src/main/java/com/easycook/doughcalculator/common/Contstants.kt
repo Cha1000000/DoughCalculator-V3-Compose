46c76a30008e2fdeedb014258c394ea1328b5289
Cha1000000 <racerkafa@gmail.com>
2024-05-15 09:29:13 +0300

    add androidx-compose navigation; init NavGraph

---
+++ app/src/main/java/com/easycook/doughcalculator/common/Contstants.kt
@@ -0,0 +1,5 @@
+package com.easycook.doughcalculator.common
+
+const val RECIPES_SCREEN = "recipes_list_screen"
+const val CALCULATION_SCREEN = "calculation_screen"
+const val SAVE_RECIPE_SCREEN = "save_recipe_screen"
---
+++ app/src/main/java/com/easycook/doughcalculator/navigation/NavGraph.kt
@@ -0,0 +1,31 @@
+package com.easycook.doughcalculator.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.easycook.doughcalculator.common.CALCULATION_SCREEN
+import com.easycook.doughcalculator.common.RECIPES_SCREEN
+import com.easycook.doughcalculator.common.SAVE_RECIPE_SCREEN
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    paddingValues: PaddingValues,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = RECIPES_SCREEN
+    ) {
+        composable(RECIPES_SCREEN) {
+            //RecipesScreen(navController)
+        }
+        composable(CALCULATION_SCREEN) {
+            //CalculationScreen(paddingValues, navController)
+        }
+        composable(SAVE_RECIPE_SCREEN) {
+            //SaveRecipeScreen(paddingValues, navController)
+        }
+    }
+}
