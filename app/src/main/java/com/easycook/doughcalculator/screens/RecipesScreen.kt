2e88df872994d70a0d974fd4beb2f8f69d8e19f1
Cha1000000 <racerkafa@gmail.com>
2024-05-20 22:32:18 +0300

    add RecipesScreen background image; add FAB to add new recipe

--- app/src/main/java/com/easycook/doughcalculator/screens/RecipesScreen.kt
+++ app/src/main/java/com/easycook/doughcalculator/screens/RecipesScreen.kt
@@ -1,8 +1,9 @@
 package com.easycook.doughcalculator.screens
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,12 +12,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.AlertDialog
+import androidx.compose.material3.FloatingActionButton
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
@@ -39,7 +43,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -53,11 +60,9 @@ import com.easycook.doughcalculator.common.CALCULATION_SCREEN
 import com.easycook.doughcalculator.database.DoughRecipeEntity
 import com.easycook.doughcalculator.ui.theme.DoughCalculatorTheme
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun RecipesScreen(
     navController: NavHostController,
-    //paddingValues: PaddingValues,
     viewModel: RecipeViewModel = hiltViewModel()
 ) {
     val recipes = viewModel.recipes.collectAsState(initial = emptyList())
@@ -73,23 +78,34 @@ fun RecipesScreen(
                 backgroundColor = colorScheme.primary,
             )
         },
-        //floatingActionButton = { AddRecipeButton(navController) }
-    ) {
+        floatingActionButton = { AddRecipeButton(navController) }
+    ) { paddingValues ->
         val state = rememberLazyListState()
-        LazyColumn(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(start = 12.dp, top = 78.dp, end = 12.dp, bottom = 20.dp),
-            //verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            state = state,
+                .padding(paddingValues)
         ) {
-            itemsIndexed(
-                items = recipes.value,
-                // Provide a unique key based on the item content
-                key = { _, item -> item.hashCode() }
-            ) { _, item ->
-                RecipeItem(item, navController)
+            Image(
+                painter = painterResource(id = R.drawable.background_notepad),
+                contentDescription = "Background image",
+                modifier = Modifier.matchParentSize(),
+                contentScale = ContentScale.FillBounds
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 24.dp, top = 18.dp, end = 12.dp, bottom = 20.dp),
+                horizontalAlignment = CenterHorizontally,
+                state = state,
+            ) {
+                itemsIndexed(
+                    items = recipes.value,
+                    // Provide a unique key based on the item content
+                    key = { _, item -> item.hashCode() }
+                ) { _, item ->
+                    RecipeItem(item, navController)
+                }
             }
         }
     }
@@ -108,7 +124,7 @@ fun RecipeItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp, horizontal = 4.dp),
+            .padding(vertical = 6.dp, horizontal = 4.dp),
         shape = RoundedCornerShape(12.dp),
         colors = CardDefaults.cardColors(cardBackground),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
@@ -200,5 +216,20 @@ fun ShowConfirmDialog(
                 },
             )
         }
+    }
+}
+
+@Composable
+fun AddRecipeButton(navController: NavHostController) {
+    FloatingActionButton(
+        modifier = Modifier.padding(vertical = 8.dp),
+        onClick = { navController.navigate(CALCULATION_SCREEN) },
+        shape = CircleShape,
+        containerColor = colorScheme.primary
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = "Add new recipe",
+        )
     }
 }
